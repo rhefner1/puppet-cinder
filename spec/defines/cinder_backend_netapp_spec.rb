7ad8f72272cd736b7cb506721b41478e7bc9992f
Ryan Hefner <ryan.hefner@netapp.com>
2014-11-13 14:43:06 -0500

    Automates generation of NFS config file

--- spec/defines/cinder_backend_netapp_spec.rb
+++ spec/defines/cinder_backend_netapp_spec.rb
@@ -26,7 +26,7 @@ describe 'cinder::backend::netapp' do
       :expiry_thres_minutes         => '720',
       :thres_avl_size_perc_start    => '20',
       :thres_avl_size_perc_stop     => '60',
-      :nfs_shares_config            => '',
+      :nfs_shares_config            => '/etc/cinder/shares.conf',
       :netapp_copyoffload_tool_path => '',
       :netapp_controller_ips        => '',
       :netapp_sa_password           => '',
@@ -76,5 +76,15 @@ describe 'cinder::backend::netapp' do
     }) }
 
     it { should contain_cinder_config("#{req_params[:volume_backend_name]}/use_multipath_for_image_xfer").with_value('true') }
+  end
+
+  context 'with NFS shares provided' do
+    let (:req_params) { params.merge!({
+        :nfs_shares => ['10.0.0.1:/test1', '10.0.0.2:/test2'],
+    }) }
+
+    it 'writes NFS shares to file' do
+      should contain_file("#{default_params[:nfs_shares_config]}")
+    end
   end
 end
